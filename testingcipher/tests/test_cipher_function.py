82cb8c84b0e45403329dd48c08c8f773478ebda5
Cindy Chen <chencindyj@gmail.com>
2022-10-28 14:25:15 -0400

    Add test to test folder

---
+++ testingcipher/tests/test_cipher_function.py
@@ -0,0 +1,18 @@
+def cipher(text, shift, encrypt=True):
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    new_text = ''
+
+    assert type(shift) == int, "shift cannot be a string input"
+
+    for c in text:
+        index = alphabet.find(c)
+        if index == -1:
+            new_text += c
+        else:
+            new_index = index + shift if encrypt == True else index - shift
+            new_index %= len(alphabet)
+            new_text += alphabet[new_index:new_index+1]
+    return new_text
+
+def test_cipher_function():
+    assert cipher("wallaby", 26) == "WALLABY", "cipher() function doesn't work correctly"
